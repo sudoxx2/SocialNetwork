a1d17038bd1bba429f79c25aa224a73595116cb0
sudoxx2 <sudoxx2@gmail.com>
2019-12-30 02:41:17 -0800

    added routes with express router

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
     res.send("Testing endpoint home")
 })
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 })
