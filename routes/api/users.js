f625aa235458966ea4d611fbf460533a702604b0
sudoxx2 <sudoxx2@gmail.com>
2019-12-30 15:29:40 -0800

    JWT implemented on server side

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -47,13 +49,29 @@ router.post('/',[
       password
     })
 
+    // Hash password
     const salt = await bcrypt.genSalt(10)
 
     user.password = await bcrypt.hash(password, salt)
 
     await user.save();
 
-    res.send('User Registered')
+    // Create JWT and send back to client if valid
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000},
+      (err, token) => {
+      if(err) throw err
+        res.json({ token })
+      }
+    )
   } catch(err) {
     console.error(err.message)
     res.status(500).send('Server Error')
